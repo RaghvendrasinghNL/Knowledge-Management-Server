87377f877513727cc63aafd0480919866753ade7
Jainshubham01 <47806935+Jainshubham01@users.noreply.github.com>
2019-03-05 19:54:16 +0530

    Login API created

---
+++ KnowledgeManagement/KnowledgeManagement/Models/LoginRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnowledgeManagement.Models
+{
+    public class LoginRequestModel
+    {
+        public string LoginToken { get; set; }
+        public string EmailId { get; set; }
+    }
+}
---
+++ KnowledgeManagement/KnowledgeManagement/Services/AccountService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KnowledgeManagement.Models;
+
+namespace KnowledgeManagement.Services
+{
+    public class AccountService
+    {
+        private readonly KnowledgeManagementDevEntities db;
+
+        public AccountService() {
+            db = new KnowledgeManagementDevEntities();
+        }
+
+        public bool AddUserLoginToken(LoginRequestModel loginRequestModel) {
+            var user = db.Users.Where(w => w.EmailId == loginRequestModel.EmailId).FirstOrDefault();
+
+            if (user == null)
+            {
+                return false;
+            }
+            user.Token = loginRequestModel.LoginToken;
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return true;
+        }
+
+        private void test() {
+            var model = new LoginRequestModel();
+            model.EmailId = "email";
+            model.LoginToken = null;
+
+            var accountService = new AccountService();
+            accountService.AddUserLoginToken(model);
+        }
+
+    }
+}
