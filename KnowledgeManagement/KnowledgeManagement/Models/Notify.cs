e4de38e6117daf78bf6742f21d0b8626740afee6
Raghvendra <raghvendra.singh@nineleaps.com>
2019-04-01 17:30:25 +0530

    changes in notification

---
+++ KnowledgeManagement/KnowledgeManagement/Models/Notify.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnowledgeManagement.Models
+{
+    public class Notify
+    {
+        public int Type { get; set; }
+        public int IsRead { get; set; }
+        public string PostName { get; set; }
+        public string DoneBy { get; set; }
+    }
+}
