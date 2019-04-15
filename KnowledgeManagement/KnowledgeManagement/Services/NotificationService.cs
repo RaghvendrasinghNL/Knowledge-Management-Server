96aad53c8b0d7ea0b605e1cf8997989c4a0fd34e
Jainshubham01 <47806935+Jainshubham01@users.noreply.github.com>
2019-04-15 14:49:17 +0530

    Updated notification api

--- KnowledgeManagement/KnowledgeManagement/Services/NotificationService.cs
+++ KnowledgeManagement/KnowledgeManagement/Services/NotificationService.cs
@@ -13,53 +13,34 @@ namespace KnowledgeManagement.Services
     {
         KnowledgeManagementDevEntities db = new KnowledgeManagementDevEntities();
 
-        public NotificationModel GetNotificationById(int Id)
+
+
+        public List<Notify> GetUserNotification(int UserId)
         {
-            var result = db.Notifications.Where(w => w.NotificationId == Id).Select(w =>
-                         new NotificationModel
-                         {
-                             Type = w.NotificationType,
-                             IsRead=w.IsRead
+            var user = db.Users.FirstOrDefault(u => u.UserId == UserId);
+            var posts = (from p in db.Posts
+                         where p.UserId == user.UserId
+                         select p).ToList();
 
-                         }
-            ).FirstOrDefault();
-
-            return result;
-        }
-
-        public List<string> GetUserNotification(string token)
-        {
-            var user = db.Users.FirstOrDefault(u => u.Token == token);
-            var post = db.Posts.Where(p => p.UserId == user.UserId);
-            List<Notify> notification = new List<Notify>();
-            foreach (var p in post)
+            //db.Posts.Where(p => p.UserId == user.UserId);
+            List<Notify> notifs = new List<Notify>();
+            foreach (var p in posts)
             {
-                var notifications = from n in db.Notifications
-                            join u in db.Users on n.UserId equals u.UserId
-                            where n.PostId == p.PostId
-                            select new Notify
-                            {
-                                Type = n.NotificationType,
-                                IsRead = n.IsRead,
-                                PostName = p.Title,
-                                DoneBy = u.FirstName
-                            };
-                foreach(var notif in notifications)
-                {
-                    notification.Add(notif);
-                }
+                var notifications = (from n in db.Notifications
+                                     join u in db.Users on n.UserId equals u.UserId
+                                     where n.PostId == p.PostId
+                                     select new Notify
+                                     {
+                                         Type = n.NotificationType,
+                                         IsRead = n.IsRead,
+                                         PostName = p.Title,
+                                         DoneBy = u.FirstName
+                                     }).ToList();
+
+                notifs.AddRange(notifications);
             }
 
-            List<string> UserNotifications = new List<string>();
-
-            foreach (var notif in notification)
-            {
-                var status = (notif.Type == 1) ? "Liked" : "Commennt";
-                string str = "Your post named: " + notif.PostName + " is " + status + " by " + notif.DoneBy;
-                UserNotifications.Add(str);
-            }
-
-            return UserNotifications;
+            return notifs;
 
         }
 
