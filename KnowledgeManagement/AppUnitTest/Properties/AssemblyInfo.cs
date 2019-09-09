6296a2e8aa0f2d78338a55507c49a26c68eda680
sahurjt <rajat.sahu@nineleaps.com>
2019-09-09 17:50:34 +0530

    added unit test proj

---
+++ KnowledgeManagement/AppUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8613962-8f45-4e5c-a32a-11db4cedaa38")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KnowledgeManagement/AppUnitTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            /// arrange or mock
+            var a = 1;
+            var b = 4;
+            var expected = 5;
+
+            // Act
+            var result = Sum(a, b);
+
+            /// Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        private int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
